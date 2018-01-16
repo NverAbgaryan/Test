bb6b9be05ee5e654ca1f04e4c2232ce6506393ec
Nver Abgaryan <abgaryannver1993@gmail.com>
2018-01-16 23:24:33 +0400

    add swagger

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-render-view')
-const path = require('path')
 const serve = require('koa-static')
 const server = require('http').createServer(app.callback())
+const swagger = require('swagger-injector')
+require('dotenv').config()
 let io = require('socket.io')(server)
 
 require('./socket/socket')(io)
 require('dotenv').config()
 require('./game/game')
-
-app.use(views(path.join(__dirname, '/dist')))
+console.log(process.env.APP_ADDRESS,process.env.APP_PORT)
+app.use(swagger.koa({
+  path: './api/swagger.json',
+  route: '/swagger',
+  host: process.env.APP_ADDRESS + ":" + process.env.APP_PORT,
+}))
 app.use(serve('.'));
-app.use(async function ({render}, next) {
-    await render('index')
-})
 server.listen(process.env.APP_PORT, function () {
     console.log("Your server is listening on port", process.env.APP_PORT)
 })
